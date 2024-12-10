b5ce716ff5d45a82d7bdd9e068064534eb7deddb
samuel <samuel.li2@student.tdsb.on.ca>
2024-12-10 13:54:02 -0500

    added fire base

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyB7KPS6SQyUBClvxxMye2gYbQjQe-FRK-M",
+    authDomain: "summative-d31ff.firebaseapp.com",
+    projectId: "summative-d31ff",
+    storageBucket: "summative-d31ff.firebasestorage.app",
+    messagingSenderId: "477780856282",
+    appId: "1:477780856282:web:e6c92b38ed29288d23c4e3"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
